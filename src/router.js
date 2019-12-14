3cacf9a6840628e93f2a36c1fc83a7221223134a
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-14 19:57:21 +0300

    Update router.js: add home and NewProject

--- src/router.js
+++ src/router.js
@@ -4,15 +4,19 @@ import Deneme from "./components/deneme.vue";
 import Signin from "./components/sign-in.vue";
 import SourceCompile from "./components/source-compile.vue"
 import Login from "./components/login.vue"
+import Home from "./components/home.vue"
+import NewProject from "./components/newProject"
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {path: '/', name: 'home', component: Home},
     {path: '/deneme', name: 'deneme', component: Deneme},
     {path: '/signin', name: 'signin', component: Signin},
     {path: '/source-compile', name: 'source-compile', component: SourceCompile},
     {path: '/login', name: 'login', component: Login},
-
-  ]
+    {path: '/new-project', name: 'newProject', component: NewProject}
+  ],
+  mode: 'history'
 });
