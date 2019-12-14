810a03bf5f851e92cafa63f004d7e5e63d05d8de
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-14 23:24:21 +0300

    Update jobProjects.vue: connect to API

--- src/components/jobProjects.vue
+++ src/components/jobProjects.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="container">
             <h2 style="text-align:center; margin-top:20px">İş İlan Projeleri</h2>
-      <div class="row" style="padding-top:20px">
-            <div class="col-md-12">
+      <div class="row" style="padding-top:20px" v-for="item in projects">
+            <div class="col-md-12" >
               <div class="card">
                 <div class="card-body">
                   <div class="row">
                     <div class="col-md-6">
-                      <h3 class="card-title" style="text-align:left;">Project Title</h3>
+                      <h3 class="card-title" style="text-align:left;">{{item.title}}</h3>
                     </div>
                     <div class="col-md-6">
                       <h5 class="card-title" style="text-align:right;">Gaga Software</h5>
@@ -19,21 +19,19 @@
                   </div>
                   <div class="row">
                     <div class="col-md-12">
-                      <div class="card-text" style="text-align:left;display: block;height:50px;overflow: hidden;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis tellus, luctus at dui ut, faucibus eleifend ligula. Quisque
-                        convallis placerat luctus. Integer euismod odio sed blandit luctus. Cras ac lobortis quam, eu molestie turpis. Duis efficitur, est quis tincidunt aliquet, odio est vehicula sem, quis gravida metus ligula id neque. Praesent
-                        lobortis diam ut leo semper pretium at nec velit. Ut ac lobortis sem. Cras cursus convallis dictum. In eu erat non est ullamcorper cursus.Pellentesque efficitur, metus non interdum volutpat, metus nulla molestie erat, ut cunon
-                        interdum volutpat, metus nulla molestie erat, ut cunon interdum volutpat, metus nulla molestie erat, ut cursus leo dui eget urna.
+                      <div class="card-text" style="text-align:left;display: block;height:50px;overflow: hidden;">
+                        {{item.description}}
                       </div>
                     </div>
                   </div>
                   <div class="row" style="margin-top: 20px">
                     <div class="col-md-10">
                       <div class="card-title">
-                        Languages: c#
+                        Languages: {{item.languages}}
                       </div>
                     </div>
                     <div class="col-md-2">
-                      <a href="#" class="btn btn-primary" style="text-align:left">Go somewhere</a>
+                      <a href="#" class="btn btn-primary" style="text-align:left">Proje Detayı</a>
                     </div>
 
                   </div>
@@ -41,45 +39,6 @@
               </div>
             </div>
           </div>
-          <div class="row" style="padding-top:20px">
-                <div class="col-md-12">
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-md-6">
-                          <h3 class="card-title" style="text-align:left;">Project Title</h3>
-                        </div>
-                        <div class="col-md-6">
-                          <h5 class="card-title" style="text-align:right;">Gaga Software</h5>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="card-title" style="text-align:left;">Kalan Süre: <span style="margin-left:10px"> asdfasdf </span> </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="card-text" style="text-align:left;display: block;height:50px;overflow: hidden;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis tellus, luctus at dui ut, faucibus eleifend ligula. Quisque
-                            convallis placerat luctus. Integer euismod odio sed blandit luctus. Cras ac lobortis quam, eu molestie turpis. Duis efficitur, est quis tincidunt aliquet, odio est vehicula sem, quis gravida metus ligula id neque. Praesent
-                            lobortis diam ut leo semper pretium at nec velit. Ut ac lobortis sem. Cras cursus convallis dictum. In eu erat non est ullamcorper cursus.Pellentesque efficitur, metus non interdum volutpat, metus nulla molestie erat, ut cunon
-                            interdum volutpat, metus nulla molestie erat, ut cunon interdum volutpat, metus nulla molestie erat, ut cursus leo dui eget urna.
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row" style="margin-top: 20px">
-                        <div class="col-md-10">
-                          <div class="card-title">
-                            Languages: c#
-                          </div>
-                        </div>
-                        <div class="col-md-2">
-                          <a href="#" class="btn btn-primary" style="text-align:left">Go somewhere</a>
-                        </div>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
     </div>
   </div>
 </template>
@@ -94,12 +53,19 @@ import axios from 'axios'
 
 export default {
   name: 'Jobprojects',
-  mounted: function(){
+  data() {
+        return {
+            projects: []
+        }
+    },
+  created(){
+    var self = this;
     let url = "http://localhost:3000/job-project/list"
 
      axios.get(url)
-     .then(function (response) {
-       console.log(response)
+     .then(function (res) {
+       self.projects = res.data
+       console.log("Projects: ",res.data)
      })
      .catch(function (error) {
          console.log("error", error)
